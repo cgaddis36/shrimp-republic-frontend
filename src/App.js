a66140fda91b3413f70aa3172f1dc4f31796daf8
Chase Gaddis <cgaddis36@gmail.com>
2022-12-15 10:31:02 -0600

    remove currentUser state

--- src/App.js
+++ src/App.js
@@ -6,19 +6,17 @@ import Soft from './components/screens/soft'
 import Hard from './components/screens/hard'
 
 function App() {
-  const [currentUser, setCurrentUser] = useState('')
-
   return (
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<Splash currentUser={currentUser}/>}/>
-          <Route path="/softgoods" element={<Soft currentUser={currentUser}/>}/>
-          <Route path="/hardgoods" element={<Hard />}/>
+          <Route path="/" element={<Splash/>}/>
+          <Route path="/softgoods" element={<Soft/>}/>
+          <Route path="/hardgoods" element={<Hard/>}/>
         </Routes>
       </Router>
     </div>
-  );
+  )
 }
 
 export default App
