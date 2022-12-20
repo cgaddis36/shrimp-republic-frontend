deec6ff129c1fa4f2dbc5267580c6ea0e8aa51ae
Chase Gaddis <cgaddis36@gmail.com>
2022-12-19 19:26:56 -0600

    update Hardgoods screen

--- src/components/screens/hard.js
+++ src/components/screens/hard.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from '../styles/hard.module.css'
-function Hard({
+import FindItems from '../apiCalls/findItems.js'
+import HardItemBox from '../features/hardItemBox.js'
+import Header from '../features/header'
 
+function Hard({
+  
 }) {
+  const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(true)
+  const role = 'hardgoods'
+  useEffect(() => {
+    FindItems({role, setLoading, setItems})
+  }, [])
   return (
     <div className={styles.mainContainer}>
-      Hardgoods
+      <Header 
+        active={'hard'}
+        />
+        <div className={styles.filters}>
+          <div className={styles.filter}>
+            Signs 
+          </div>
+          <div className={styles.filter}>
+            Coasters 
+          </div>
+        </div>
+      <div className={styles.items}>
+        {
+          !items.length ?
+          <div className={styles.loading}>
+          </div> 
+          : 
+          items.map((item, index)=> 
+            <HardItemBox
+              key={index}
+              name={item.name}
+              description={item.description}
+              price={item.price}  
+              discount={item.discount}  
+              />)
+        }
+      </div>
     </div>
   )
 }
