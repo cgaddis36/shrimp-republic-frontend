e0a2ce07657a19128a520772fa3c01e3c663231e
Chase Gaddis <cgaddis36@gmail.com>
2022-12-19 18:33:36 -0600

    add logo to header

--- src/components/features/header.js
+++ src/components/features/header.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import NavBar from './navBar'
 import styles from '../styles/header.module.css'
-
+import flyFishCircleLogo from '../images/flyFishCircleLogo.png'
+import { useNavigate } from 'react-router-dom'
 function Header({
   active
 }){
+  const navigate = useNavigate()
   return(
     <div className={styles.header}>
-      <div 
-        className={styles.logoContainer}>
-        <div className={styles.logoText}>
+      <div className={styles.logoContainer}>
+        <img 
+          src={flyFishCircleLogo}
+          className={styles.logo}
+          />
+        <div 
+          className={styles.logoText}
+          onClick={()=>navigate('/')}
+          >
           Shrimp Republic
         </div>
       </div>
